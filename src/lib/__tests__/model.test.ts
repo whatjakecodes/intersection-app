71e25b083336ce723e5526f4e2c5549494ffd714
Jake Stout <kyjstout@gmail.com>
2024-12-27 09:58:13 -0500

    Initial commit; add basic test

---
+++ src/lib/__tests__/model.test.ts
@@ -0,0 +1,46 @@
+﻿import {expect, test} from "vitest";
+import {IntersectionModel} from "$lib/intersectionModel";
+
+test("intersection model can be created", () => {
+  const subject = new IntersectionModel();
+  
+  expect(subject.northRoad.leftLane.cars).toHaveLength(0);
+  expect(subject.northRoad.midLeftLane.cars).toHaveLength(0);
+  expect(subject.northRoad.midRightLane.cars).toHaveLength(0);
+  expect(subject.northRoad.rightLane.cars).toHaveLength(0);
+
+  expect(subject.northRoad.leftTrafficLight).toEqual('orange');
+  expect(subject.northRoad.midLeftTrafficLight).toEqual('green');
+  expect(subject.northRoad.midRightTrafficLight).toEqual('green');
+  expect(subject.northRoad.rightTrafficLight).toEqual('red');
+
+  expect(subject.southRoad.leftLane.cars).toHaveLength(0);
+  expect(subject.southRoad.midLeftLane.cars).toHaveLength(0);
+  expect(subject.southRoad.midRightLane.cars).toHaveLength(0);
+  expect(subject.southRoad.rightLane.cars).toHaveLength(0);
+  
+  expect(subject.southRoad.leftTrafficLight).toEqual('orange');
+  expect(subject.southRoad.midLeftTrafficLight).toEqual('green');
+  expect(subject.southRoad.midRightTrafficLight).toEqual('green');
+  expect(subject.southRoad.rightTrafficLight).toEqual('red');
+
+  expect(subject.eastRoad.leftLane.cars).toHaveLength(0);
+  expect(subject.eastRoad.midLeftLane.cars).toHaveLength(0);
+  expect(subject.eastRoad.midRightLane.cars).toHaveLength(0);
+  expect(subject.eastRoad.rightLane.cars).toHaveLength(0);
+  
+  expect(subject.eastRoad.leftTrafficLight).toEqual('orange');
+  expect(subject.eastRoad.midLeftTrafficLight).toEqual('green');
+  expect(subject.eastRoad.midRightTrafficLight).toEqual('green');
+  expect(subject.eastRoad.rightTrafficLight).toEqual('red');
+
+  expect(subject.westRoad.leftLane.cars).toHaveLength(0);
+  expect(subject.westRoad.midLeftLane.cars).toHaveLength(0);
+  expect(subject.westRoad.midRightLane.cars).toHaveLength(0);
+  expect(subject.westRoad.rightLane.cars).toHaveLength(0);
+  
+  expect(subject.westRoad.leftTrafficLight).toEqual('orange');
+  expect(subject.westRoad.midLeftTrafficLight).toEqual('green');
+  expect(subject.westRoad.midRightTrafficLight).toEqual('green');
+  expect(subject.westRoad.rightTrafficLight).toEqual('red');
+});
---
+++ src/lib/intersectionModel.ts
@@ -0,0 +1,48 @@
+﻿interface Car {
+}
+
+interface Lane {
+  cars: Car[];
+}
+
+type TrafficLightState = 'red' | 'green' | 'yellow';
+type LeftTrafficLightState = TrafficLightState | 'orange';
+
+interface Road {
+  midLeftTrafficLight: TrafficLightState;
+  midRightTrafficLight: TrafficLightState;
+  leftTrafficLight: LeftTrafficLightState;
+  rightTrafficLight: TrafficLightState;
+
+  rightLane: Lane;
+  midRightLane: Lane;
+  midLeftLane: Lane;
+  leftLane: Lane;
+}
+
+export class IntersectionModel {
+  northRoad: Road;
+  southRoad: Road;
+  eastRoad: Road;
+  westRoad: Road;
+
+  constructor() {
+
+    const defaultRoad: Road = {
+      leftTrafficLight: "orange",
+      midLeftTrafficLight: "green",
+      midRightTrafficLight: "green",
+      rightTrafficLight: "red",
+
+      leftLane: {cars: []},
+      midRightLane: {cars: []},
+      midLeftLane: {cars: []},
+      rightLane: {cars: []}
+    };
+
+    this.northRoad = {...defaultRoad};
+    this.southRoad = {...defaultRoad};
+    this.eastRoad = {...defaultRoad};
+    this.westRoad = {...defaultRoad};
+  }
+}
